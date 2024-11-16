8f21db0dd370a426effe360905e3d1cfe2ed8ec0
elvioladordemark <168953399+elvioladordemark@users.noreply.github.com>
2024-11-16 17:10:15 -0300

    Create hora.php

---
+++ hora.php
@@ -0,0 +1,10 @@
+<?php
+// Configurar la zona horaria a Asunción, Paraguay
+date_default_timezone_set('America/Asuncion');
+
+// Obtener la hora actual
+$horaActual = date('Y-m-d H:i:s');
+
+// Mostrar la hora
+echo "$horaActual";
+?>
